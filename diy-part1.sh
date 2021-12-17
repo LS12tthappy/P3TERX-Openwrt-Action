b48086ac511f0e4529a2831ed67b5e906758d299
Ls12 <github@ls12.me>
2021-12-17 11:20:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,6 +25,6 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean
 #echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 
 # Add a Passwall
-echo "src-git passwall src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
